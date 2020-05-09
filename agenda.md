9bf1fe098bbcbf00cfe679d26b9529e45af84b74
kiran <kiran@neostack.com>
2020-05-09 12:10:20 +0530

    basic todo structure

---
+++ agenda.md
@@ -0,0 +1,9 @@
+Setting up react project
+Fundamentals of ReactJS
+JSX
+Functional components
+Props & State
+Unidirectional Dataflow in React
+Side effects with hooks
+State management with context
+Deploying to Netlify
--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+const todos = [
+  { id: 1, title: "Learn react" },
+  { id: 2, title: "Learn css" },
+];
+
+function TodoInput() {
+  return <input className="new-todo" placeholder="what needs to be done?" />;
+}
+
+function Todo({ todo }) {
+  return (
+    <li>
+      <div className="view">
+        <input
+          className="toggle"
+          type="checkbox"
+          // checked={this.props.todo.completed}
+          // onChange={this.props.onToggle}
+        />
+        <label>{todo.title}</label>
+        <button className="destroy" />
+      </div>
+    </li>
+  );
+}
+
+function TodoList() {
+  return (
+    <section className="main">
+      <input
+        id="toggle-all"
+        className="toggle-all"
+        type="checkbox"
+        // onChange={this.toggleAll}
+        // checked={activeTodoCount === 0}
+      />
+      <label htmlFor="toggle-all" />
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <Todo todo={todo} />
+        ))}
+      </ul>
+    </section>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <section className="todoapp">
+      <header className="header">
+        <h1>todos</h1>
+        <TodoInput />
       </header>
-    </div>
+      <TodoList />
+    </section>
   );
 }
 
